229024f3bc9edb3b72343d764d4b8814542f6c7f
Cristhian Camilo Beltran Ramirez <crcbeltranra@gmail.com>
2024-06-20 07:42:21 -0500

    linked most of the routes with thediferent components and fixed global styles

--- src/app/componentes/administrar-citas/administrar-citas.component.ts
+++ src/app/componentes/administrar-citas/administrar-citas.component.ts
@@ -1,10 +1,223 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PeticionService } from 'src/app/servicios/peticion.service';
+
+declare var $:any
+declare var Swal:any
 
 @Component({
   selector: 'app-administrar-citas',
   templateUrl: './administrar-citas.component.html',
   styleUrls: ['./administrar-citas.component.css']
 })
-export class AdministrarCitasComponent {
+export class AdministrarCitasComponent implements OnInit{
 
+    ngOnInit(): void {
+    this.CargarTodasusuarios()
+  }
+
+    constructor(private peticion:PeticionService){}
+    usuario:String = ""
+    clave:String = ""
+    nombre:String = ""
+    apellidos:String = ""
+    cedula:String = ""
+    correo:String = ""
+    telefono:String = ""
+    estado:Number = 1
+    codigoact:String = ""
+    rol:Number = 3
+    datos:any[]=[]
+    datosusuarios:any[]=[]
+    Idseleccionado:string=""
+
+/**
+ * funcion para cargar datos de las usuarios
+ */
+  CargarTodasusuarios(){
+
+    let post = {
+      Host:this.peticion.urlHost,
+      path:"/usuarios/list",
+      payload:{
+      }
+    }
+  
+    this.peticion.Post(post.Host+post.path, post.payload).then(
+      (res:any) => {
+        console.log(res)
+        this.datosusuarios=res.data
+      }
+      
+  )
+
+  }
+
+  AbrirModal(){
+
+    this.nombre = ""
+    this.apellidos = ""
+    this.cedula = ""
+    this.correo = ""
+    this.telefono = ""
+    this.estado = 1
+    this.Idseleccionado = ""
+    $('#modalnuevo').modal('show')
+  }
+  Guardar(){
+    let post = {
+      Host:this.peticion.urlHost,
+      path:"/usuarios/save",
+      payload:{
+        usuario:this.usuario,
+        clave:this.clave,
+        nombre:this.nombre,
+        apellidos:this.apellidos,
+        cedula:this.cedula,
+        correo:this.correo,
+        telefono:this.telefono,
+        estado:this.estado,
+        codigoact:this.codigoact,
+        rol:this.rol
+      }
+    }
+  
+
+    this.peticion.Post(post.Host+post.path, post.payload).then(
+      (res:any) => {
+        console.log(res)
+        if(res.state==true){
+
+          Swal.fire({
+            icon: "success",
+            title: "Que Bien!",
+            text: res.mensaje,
+          });
+
+
+          $('#modalnuevo').modal('hide')
+          this.CargarTodasusuarios()
+        }
+        else{
+
+          Swal.fire({
+            icon: "error",
+            title: "Ouch!",
+            text: res.mensaje,
+          });
+        }
+      }
+    )
+
+
+
+  }
+  /**
+   * funcion para cargar un solo registro
+   * @param id este es el identificador del producto
+   */
+  EditarId(id:string){
+    this.Idseleccionado=id
+    let post = {
+      Host:this.peticion.urlHost,
+      path:"/usuarios/listId",
+      payload:{
+        _id:id
+      }
+    }
+  
+    this.peticion.Post(post.Host+post.path, post.payload).then(
+      (res:any) => {
+        console.log(res)
+        this.usuario=res.data[0].usuario
+        this.clave=res.data[0].clave
+        this.nombre=res.data[0].nombre
+        this.apellidos=res.data[0].apellidos
+        this.cedula=res.data[0].cedula
+        this.correo=res.data[0].correo
+        this.telefono=res.data[0].telefono
+        this.estado=res.data[0].estado
+        this.codigoact=res.data[0].codigoact
+        this.rol=res.data[0].rol
+        $('#modalnuevo').modal('show')
+      }
+    )
+  }
+  Eliminar(){
+    let post = {
+      Host:this.peticion.urlHost,
+      path:"/usuarios/delete",
+      payload:{
+        _id:this.Idseleccionado
+      }
+    }
+  
+    this.peticion.Post(post.Host+post.path, post.payload).then(
+      (res:any) => {
+        console.log(res)
+        if(res.state==true){
+
+          Swal.fire({
+            icon: "success",
+            title: "Que Bien!",
+            text: res.mensaje,
+          });
+
+          $('#modalnuevo').modal('hide')
+          this.CargarTodasusuarios()
+        }
+        else{
+
+          Swal.fire({
+            icon: "error",
+            title: "Ouch!",
+            text: res.mensaje,
+          });
+        }
+      }
+    )
+  }
+  Actualizar(){
+    let post = {
+      Host:this.peticion.urlHost,
+      path:"/usuarios/update",
+      payload:{
+        usuario:this.usuario,
+        clave:this.clave,
+        nombre:this.nombre,
+        apellidos:this.apellidos,
+        cedula:this.cedula,
+        correo:this.correo,
+        telefono:this.telefono,
+        estado:this.estado,
+        codigoact:this.codigoact,
+        rol:this.rol
+      }
+    }
+  
+
+    this.peticion.Post(post.Host+post.path, post.payload).then(
+      (res:any) => {
+        console.log(res)
+        if(res.state==true){
+
+          Swal.fire({
+            icon: "success",
+            title: "Que Bien!",
+            text: res.mensaje,
+          });
+
+          $('#modalnuevo').modal('hide')
+          this.CargarTodasusuarios()
+        }
+        else{
+
+          Swal.fire({
+            icon: "error",
+            title: "Ouch!",
+            text: res.mensaje,
+          });
+        }
+      }
+    )
+  }
 }
--- src/app/componentes/administrar-tratamientos/administrar-tratamientos.component.ts
+++ src/app/componentes/administrar-tratamientos/administrar-tratamientos.component.ts
@@ -1,10 +1,192 @@
-import { Component } from '@angular/core';
+
+import { Component, OnInit } from '@angular/core';
+import { PeticionService } from 'src/app/servicios/peticion.service';
+
+declare var $:any
+declare var Swal:any
 
 @Component({
   selector: 'app-administrar-tratamientos',
   templateUrl: './administrar-tratamientos.component.html',
   styleUrls: ['./administrar-tratamientos.component.css']
 })
-export class AdministrarTratamientosComponent {
+export class AdministrarTratamientosComponent implements OnInit{
 
+    ngOnInit(): void {
+    this.CargarTodasusuarios()
+  }
+
+    constructor(private peticion:PeticionService){}
+    cod_prod:String = ""
+    nombre:String = ""
+    estado:Number = 1
+    datos:any[]=[]
+    datosusuarios:any[]=[]
+    Idseleccionado:string=""
+
+/**
+ * funcion para cargar datos de las usuarios
+ */
+  CargarTodasusuarios(){
+
+    let post = {
+      Host:this.peticion.urlHost,
+      path:"/usuarios/list",
+      payload:{
+      }
+    }
+  
+    this.peticion.Post(post.Host+post.path, post.payload).then(
+      (res:any) => {
+        console.log(res)
+        this.datosusuarios=res.data
+      }
+      
+  )
+
+  }
+
+  AbrirModal(){
+    this.cod_prod = ""
+    this.nombre = ""
+    this.estado = 1
+    this.Idseleccionado = ""
+    $('#modalnuevo').modal('show')
+  }
+  Guardar(){
+    let post = {
+      Host:this.peticion.urlHost,
+      path:"/usuarios/save",
+      payload:{
+        cod_prod:this.cod_prod,
+        nombre:this.nombre,
+        estado:this.estado,
+      }
+    }
+  
+
+    this.peticion.Post(post.Host+post.path, post.payload).then(
+      (res:any) => {
+        console.log(res)
+        if(res.state==true){
+
+          Swal.fire({
+            icon: "success",
+            title: "Que Bien!",
+            text: res.mensaje,
+          });
+
+
+          $('#modalnuevo').modal('hide')
+          this.CargarTodasusuarios()
+        }
+        else{
+
+          Swal.fire({
+            icon: "error",
+            title: "Ouch!",
+            text: res.mensaje,
+          });
+        }
+      }
+    )
+
+
+
+  }
+  /**
+   * funcion para cargar un solo registro
+   * @param id este es el identificador del producto
+   */
+  EditarId(id:string){
+    this.Idseleccionado=id
+    let post = {
+      Host:this.peticion.urlHost,
+      path:"/usuarios/listId",
+      payload:{
+        _id:id
+      }
+    }
+  
+    this.peticion.Post(post.Host+post.path, post.payload).then(
+      (res:any) => {
+        console.log(res)
+        this.cod_prod=res.data[0].cod_prod
+        this.nombre=res.data[0].nombre
+        this.estado=res.data[0].estado
+        $('#modalnuevo').modal('show')
+      }
+    )
+  }
+  Eliminar(){
+    let post = {
+      Host:this.peticion.urlHost,
+      path:"/usuarios/delete",
+      payload:{
+        _id:this.Idseleccionado
+      }
+    }
+  
+    this.peticion.Post(post.Host+post.path, post.payload).then(
+      (res:any) => {
+        console.log(res)
+        if(res.state==true){
+
+          Swal.fire({
+            icon: "success",
+            title: "Que Bien!",
+            text: res.mensaje,
+          });
+
+          $('#modalnuevo').modal('hide')
+          this.CargarTodasusuarios()
+        }
+        else{
+
+          Swal.fire({
+            icon: "error",
+            title: "Ouch!",
+            text: res.mensaje,
+          });
+        }
+      }
+    )
+  }
+  Actualizar(){
+    let post = {
+      Host:this.peticion.urlHost,
+      path:"/usuarios/update",
+      payload:{
+        cod_prod:this.cod_prod,
+        nombre:this.nombre,
+        estado:this.estado,
+      }
+    }
+  
+
+    this.peticion.Post(post.Host+post.path, post.payload).then(
+      (res:any) => {
+        console.log(res)
+        if(res.state==true){
+
+          Swal.fire({
+            icon: "success",
+            title: "Que Bien!",
+            text: res.mensaje,
+          });
+
+          $('#modalnuevo').modal('hide')
+          this.CargarTodasusuarios()
+        }
+        else{
+
+          Swal.fire({
+            icon: "error",
+            title: "Ouch!",
+            text: res.mensaje,
+          });
+        }
+      }
+    )
+  }
 }
