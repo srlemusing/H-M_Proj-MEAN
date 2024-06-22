eecef77bb72675a78de690d42f960015396af0ca
Steban Delgado <sdelgadobarrios@gmail.com>
2024-06-21 21:51:21 -0500

    Se establece coneccion entre el back y el front, solucionando el problema de CORS para el panel de clientes

--- src/app/componentes/user-menu-mis-datos/user-menu-mis-datos.component.ts
+++ src/app/componentes/user-menu-mis-datos/user-menu-mis-datos.component.ts
@@ -1,10 +1,56 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PeticionService } from 'src/app/servicios/peticion.service';
 
 @Component({
   selector: 'app-user-menu-mis-datos',
   templateUrl: './user-menu-mis-datos.component.html',
   styleUrls: ['./user-menu-mis-datos.component.css']
 })
-export class UserMenuMisDatosComponent {
+  export class UserMenuMisDatosComponent implements OnInit{
 
-}
+    ngOnInit(): void {
+      this.CargarDatos()
+    }
+
+    constructor(private peticion:PeticionService){}
+
+    usuario:string = ""
+    clave:string = ""
+    nombre:string = ""
+    apellidos:string = ""
+    cedula:string = ""
+    correo: string = ""
+    telefono: number = 3205555555
+    rol: string = ""
+
+    datos: any[] = []
+
+    CargarDatos(){
+
+      let get = {
+        Host:this.peticion.urlHost,
+        path:"/usuarios/list",
+        payload:{
+          usuario:this.usuario
+        }
+
+      }
+
+      this.peticion.Get(get.Host+get.path).then(
+        (respuesta:any)=>{
+          console.log(respuesta)
+          this.datos = respuesta.data
+        }
+      )
+
+
+    }
+
+
+
+
+
+
+  }
+
+
