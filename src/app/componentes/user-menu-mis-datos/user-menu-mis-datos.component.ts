745394690e4fac909ed07fe9dcb5cf1a33785b9e
Steban Delgado <sdelgadobarrios@gmail.com>
2024-06-22 21:54:21 -0500

    Se actualiza diseño para que sea responsivo y quede con los colores de la pagina principal

--- src/app/componentes/user-menu-mis-datos/user-menu-mis-datos.component.ts
+++ src/app/componentes/user-menu-mis-datos/user-menu-mis-datos.component.ts
@@ -8,13 +8,10 @@ declare var $:any
   styleUrls: ['./user-menu-mis-datos.component.css']
 })
   export class UserMenuMisDatosComponent implements OnInit{
-
     ngOnInit(): void {
       this.CargarDatos()
     }
-
     constructor(private peticion:PeticionService){}
-
     usuario:string = ""
     clave:string = ""
     nombre:string = ""
@@ -23,8 +20,8 @@ declare var $:any
     correo: string = ""
     telefono: any = 3205555555
     rol: string = ""
-
     datos: any[] = []
+    IdSeleccionado:string = ""
 
     CargarDatos(){
       let get = {
@@ -52,10 +49,10 @@ declare var $:any
       this.correo  = ""
       this.telefono = ""
       this.rol  = ""
+      this.IdSeleccionado = ""
       $('#ModalDatos').modal('show')
     }
     Guardar(){
-
       let post = {
         Host:this.peticion.urlHost,
         path:"/usuarios/save",
@@ -69,20 +66,87 @@ declare var $:any
           telefono:this.telefono,
           rol:this.rol
         }
-
       }
-
       this.peticion.Post(post.Host+post.path, post.payload).then(
         (respuesta:any)=>{
           console.log(respuesta)
+          if (respuesta.state == true){
+            $('#ModalDatos').modal('hide')
+          }
           this.CargarDatos
         }
       )
+    }
+    EditarId(id:string){
+      this.IdSeleccionado = id
+      let post = {
+        Host:this.peticion.urlHost,
+        path:"/usuarios/listId",
+        payload:{
+          _id:id
+        }
+      }
+      this.peticion.Post(post.Host+post.path, post.payload).then(
+        (respuesta:any)=>{
+          console.log(respuesta)
 
+          this.usuario = respuesta.data[0].usuario
+          this.clave = respuesta.data[0].clave
+          this.nombre = respuesta.data[0].nombre
+          this.apellidos = respuesta.data[0].apellidos
+          this.cedula = respuesta.data[0].cedula
+          this.correo  = respuesta.data[0].correo
+          this.telefono = respuesta.data[0].telefono
+          this.rol  = respuesta.data[0].rol
+          $('#ModalDatos').modal('show')
+        }
+      )
 
     }
-
-
+    Eliminar(){
+      let post = {
+        Host:this.peticion.urlHost,
+        path:"/usuarios/delete",
+        payload:{
+          _id:this.IdSeleccionado
+        }
+      }
+      this.peticion.Post(post.Host+post.path, post.payload).then(
+        (respuesta:any)=>{
+          console.log(respuesta)
+          if (respuesta.state == true){
+            $('#ModalDatos').modal('hide')
+          }
+          this.CargarDatos
+        }
+      )
+    }
+    Actualizar(){
+      let post = {
+        Host:this.peticion.urlHost,
+        path:"/usuarios/update",
+        payload:{
+          _id:this.IdSeleccionado,
+          usuario:this.usuario,
+          clave:this.clave,
+          nombre:this.nombre,
+          apellidos:this.apellidos,
+          cedula:this.cedula,
+          correo:this.correo,
+          telefono:this.telefono,
+          rol:this.rol
+        }
+      }
+      this.peticion.Post(post.Host+post.path, post.payload).then(
+        (respuesta:any)=>{
+          console.log(respuesta)
+          if (respuesta.state == true){
+            $('#ModalDatos').modal('hide')
+          }
+          this.CargarDatos
+        }
+      )
+    }
   }
 
 
